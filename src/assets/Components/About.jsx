cb52a6686d1db9ad83f55991fe8ec4ecb7576300
bia-pequeno <93928658+bia-pequeno@users.noreply.github.com>
2024-11-13 00:27:30 -0300

    Update About.jsx

--- src/assets/Components/About.jsx
+++ src/assets/Components/About.jsx
@@ -11,7 +11,7 @@ function About() {
             </div>
             <div className={Styles.mebea}> 
                 <h2>Sobre Mim</h2>
-                <p>Me chamo Beatriz, graduando o curso de Analise e Desenvolvimento de Sistemas pela Impacta. Desenvolvo páginas e sistemas web responsívos e de layout atrativo. Tenho uma trajetória de cerca de 4 anos estudando programação com cursos técnicos. Hoje na área de desenvolvimento vivo o aprendizado continuo, focando em tecnologias JavaScript e Python. Busco oportunidades no mercado como dev e, em um futuro próximo, viso me tornar um desenvolvedor Full Stack. </p>
+                <p>Me chamo Beatriz, graduando o curso de Analise e Desenvolvimento de Sistemas pela Impacta. Desenvolvo páginas e sistemas web responsívos e de layout atrativo. Tenho uma trajetória de cerca de 4 anos estudando programação com cursos técnicos. Hoje na área de desenvolvimento vivo o aprendizado continuo, focando em tecnologias JavaScript e Python. Busco oportunidades no mercado como dev e, em um futuro próximo, viso me tornar um desenvolvedora Full Stack. </p>
             </div>
         </section>
         </>
